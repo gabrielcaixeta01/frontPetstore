6d6cdfedb4a3b17f89fbde302fb4d4bf9408209b
Gabriel Caixeta Romero <gabrielcaixetaromero@gmail.com>
2026-04-13 17:38:18 -0300

    removendo login por enquanto

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { apexTheme } from "./lib/theme";
 import AppNavbar from "./components/AppNavbar";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
-import LoginPage from "./pages/LoginPage";
 import OrdersPage from "./pages/OrdersPage";
 import PetsPage from "./pages/PetsPage";
 import TagsPage from "./pages/TagsPage";
 import UsersPage from "./pages/UsersPage";
 
 function App() {
+  const c = apexTheme.colors;
+
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-[#071c1b]">
+      <div className={`min-h-screen ${c.bg}`}>
+        <AppNavbar />
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-
-          <Route
-            path="*"
-            element={
-              <ProtectedRoute>
-                <AppNavbar />
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/pets" element={<PetsPage />} />
-                  <Route path="/users" element={<UsersPage />} />
-                  <Route path="/tags" element={<TagsPage />} />
-                  <Route path="/orders" element={<OrdersPage />} />
-                </Routes>
-              </ProtectedRoute>
-            }
-          />
+          <Route path="/" element={<Home />} />
+          <Route path="/pets" element={<PetsPage />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/tags" element={<TagsPage />} />
+          <Route path="/orders" element={<OrdersPage />} />
         </Routes>
       </div>
     </BrowserRouter>
